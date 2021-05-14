a5ce542ce79507bf3ede611b0adf854636fc259b
Everton0Sylva <cerberuzwolf.dev@gmail.com>
2021-05-14 10:01:02 -0300

    Novo component

--- src/app/view/layout/layout.routing.ts
+++ src/app/view/layout/layout.routing.ts
@@ -2,13 +2,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
+
 export const routes: Routes = [
   {
     path: 'home',
     component: LayoutComponent,
     children: [
       {
-        path: '', loadChildren: () => import('./SALE/sale.module').then(m => m.SaleModule)
       },
     ]
   },
